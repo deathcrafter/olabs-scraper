9448a9f487d33380bd6908612575dc9ddca1a300
deathcrafter <sahooshaktijeet@gmail.com>
2024-02-27 21:16:49 +0530

    fix simulator

--- pipelines/save_simulator.py
+++ pipelines/save_simulator.py
@@ -1,9 +1,12 @@
-from pathlib import Path
-import requests
-from bs4 import BeautifulSoup
 import os
-import urllib.parse
+from pathlib import Path
 import re
+import time
+from bs4 import BeautifulSoup
+import requests
+
+from selenium.webdriver import Chrome
+import urllib.parse
 
 
 def download_file(url, directory):
@@ -16,13 +19,7 @@ def download_file(url, directory):
     return local_filename
 
 
-def create_directories(directory):
-    folders = ["css", "css/assets", "js", "assets"]
-    for folder in folders:
-        os.makedirs(os.path.join(directory, folder), exist_ok=True)
-
-
-def extract_assets_from_css(css_url, original_url, directory):
+def extract_assets_from_css(css_url: str, original_url: str, directory: Path):
     css_file_name = css_url.split("/")[-1]
     response = requests.get(css_url)
     css_content = response.text
@@ -34,7 +31,7 @@ def extract_assets_from_css(css_url, original_url, directory):
         # Remove quotes and whitespace from the URL
         url = url.strip(" '\"")
         if not (url.startswith("data:") or url.startswith("http")):
-            remote_url = urllib.parse.urljoin(original_url, url)
+            remote_url = urllib.parse.urljoin(css_url, url)
             local_url = download_file(
                 remote_url, os.path.join(directory, "css", "assets")
             )
@@ -53,68 +50,73 @@ def extract_assets_from_css(css_url, original_url, directory):
     return os.path.join(directory, "css", css_file_name)
 
 
-def extract_assets(url, directory: Path):
-    response = requests.get(url)
-    original_url = response.url
-    html_content = response.text
-
-    soup = BeautifulSoup(html_content, "html.parser")
-
+def extract_assets(soup: BeautifulSoup, original_url: str, directory: Path):
     # Extracting image assets
     img_tags = soup.find_all("img")
     for img in img_tags:
         img_url = img.get("src")
-        if img_url:
-            img_url = urllib.parse.urljoin(url, img_url)
+        if img_url and not str(img_url).startswith("http"):
+            img_url = urllib.parse.urljoin(original_url, img_url)
             local_file = download_file(img_url, directory / "assets")
             img["src"] = os.path.relpath(local_file, directory).replace("\\", "/")
 
-    # Extract assets from CSS files
-    css_tags = soup.find_all("link", {"rel": "stylesheet", "href": True})
-    for css in css_tags:
-        css_url = css.get("href")
-        if css_url:
-            css_url = urllib.parse.urljoin(original_url, css_url)
-            local_file = extract_assets_from_css(css_url, original_url, directory)
-            css_href = os.path.relpath(local_file, directory)
-            css["href"] = css_href.replace("\\", "/")
-
-    # Extracting JS assets
+    # Extract JS files
     js_tags = soup.find_all("script", {"src": True})
     for js in js_tags:
         js_url = js.get("src")
-        if js_url:
-            js_url = urllib.parse.urljoin(url, js_url)
+        if js_url and not str(js_url).startswith("http"):
+            js_url = urllib.parse.urljoin(original_url, js_url)
             local_file = download_file(js_url, directory / "js")
-            js["src"] = os.path.relpath(
-                local_file,
-                directory,
-            ).replace("\\", "/")
+            js["src"] = os.path.relpath(local_file, directory).replace("\\", "/")
 
-    # Extracting audio files and other assets (modify as needed)
+    # Extract CSS files along with embedded assets
+    css_tags = soup.find_all("link", {"rel": "stylesheet", "href": True})
+    for css in css_tags:
+        css_url = css.get("href")
+        if css_url and not str(css_url).startswith("http"):
+            css_url = urllib.parse.urljoin(original_url, css_url)
+            local_file = extract_assets_from_css(css_url, original_url, directory)
+            css["href"] = os.path.relpath(local_file, directory).replace("\\", "/")
+
+    # Extracting audio files
     other_tags = soup.find_all(["audio", "source"])
     for other in other_tags:
-        other_url = other.get("src")
-        if other_url:
-            other_url = urllib.parse.urljoin(url, other_url)
-            local_file = download_file(other_url, directory / "assets")
+        audio_url = other.get("src")
+        if audio_url:
+            audio_url = urllib.parse.urljoin(original_url, audio_url)
+            local_file = download_file(audio_url, directory / "assets")
             other["src"] = os.path.relpath(local_file, directory).replace("\\", "/")
 
     if soup.footer:
         soup.footer.decompose()
 
-    text_html = soup.prettify()
-    text_html = text_html.replace(
-        'var simPath="../', 'var simPath="https:https://amrita.olabs.edu.in/olab/'
+    return soup
+
+
+def create_directories(directory):
+    folders = ["css", "css/assets", "js", "assets"]
+    for folder in folders:
+        os.makedirs(os.path.join(directory, folder), exist_ok=True)
+
+
+def save_simulator(driver: Chrome, url: str, directory: Path):
+    driver.implicitly_wait(10)
+    driver.get(url)
+
+    html = driver.execute_script(
+        "return document.getElementsByTagName('html')[0].outerHTML;"
     )
+    html = BeautifulSoup(html, "html.parser")
 
-    # Save updated HTML content to a new file
-    with open(os.path.join(directory, "index.html"), "w", encoding="utf-8") as f:
-        f.write(text_html)
-
-
-def save_simulator(url: str, directory: Path):
     if not os.path.exists(directory):
         os.makedirs(directory)
     create_directories(directory)
-    extract_assets(url, directory)
+    html = extract_assets(html, url, directory)
+
+    html = html.prettify()
+    html = html.replace(
+        'var simPath="../', 'var simPath="https://amrita.olabs.edu.in/olab/'
+    )
+
+    with open(os.path.join(directory, "index.html"), "w", encoding="utf-8") as f:
+        f.write(html)
